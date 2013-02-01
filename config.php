f54eede36dc1f7bcb2a9607a32a709d780375f25
Ali <ali.allomani@gmail.com>
2013-02-01 18:13:14 +0300

    init files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Yakimbi PHP Developer Task
+ * 
+ * @author  Ali Allomani <info@allomani.com>
+ */
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Yakimbi PHP Developer Task
+ * 
+ * @author  Ali Allomani <info@allomani.com>
+ */
+
+?>
+
